318ff97de9aab076ffc811b7d3a2ad61511fcf57
Hachem <106035779+ash-automates@users.noreply.github.com>
2024-05-02 21:01:53 +0000

    Added code for lesson 9

--- objects.js
+++ objects.js
@@ -1,36 +1,14 @@
-class User {
-    constructor (email, name) {
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-    }
-    login() {
-        console.log(this.email, 'just logged in');
-        return this;
-    }
-    logout() {
-        console.log(this.email, 'just logged out');
-        return this;
-    }
-    updateScore() {
-        this.score = this.score + 1;
-        console.log(this.email, "score now is", this.score);
-        return this;
-    }
-};
-
-class Admin extends User {
-    deleteUser(user) {
-        users = users.filter(u => user.email != u.email);
-    }
+function User(email, name) {
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login = function() {
+        console.log(this.email, 'has logged in');
+    };
 }
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-var adminOne = new Admin('shaun@ninjas.com', 'Shaun');
 
-var users = [userOne, userTwo, adminOne];
-
-adminOne.deleteUser(userOne);
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
