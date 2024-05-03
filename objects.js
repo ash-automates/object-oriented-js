a11183e271c636a0609537c6b8af5d79f690adb6
Hachem <s.hachem@murena.io>
2024-05-03 10:42:46 +0000

    Added code for lesson 10

--- objects.js
+++ objects.js
@@ -14,6 +14,20 @@ User.prototype.logout = function () {
     console.log(this.email, 'has logged out');
 }
 
+function Admin(...args) {
+    User.apply(this, args);
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function (userToDel) {
+    users = users.filter(user => {
+        return userToDel.email != user.email;
+    });
+}
+
+var users = [userOne, userTwo, adminOne];
+var adminOne = new Admin('shaun@ninjas.com', 'Shaun');
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
 
